276aa78f563459ca53f242104c244e78b79930f5
deashadow <67285924+deashadow@users.noreply.github.com>
2021-02-11 12:42:18 -0500

    added while loops and break statements to add error statements

--- mortgageCalculator.java
+++ mortgageCalculator.java
@@ -6,23 +6,40 @@ public class mortgageCalculator {
         final byte MONTHS_IN_YEAR = 12;
         final byte PERCENT = 100;
 
+        int principal = 0; //variable must be outside of while loop
+        float monthlyInterest = 0;
+        int numberOfPayments = 0;
+
         Scanner scanner = new Scanner(System.in);
-
+    while (true) {
         System.out.print("Principal: ");
-        int principal = scanner.nextInt();
-
+        principal = scanner.nextInt();
+        if (principal >= 5000 && principal <= 1_000_000)
+            break;
+        System.out.println("Please enter a valid dollar value between 5000 and 1000000");
+    }
+    while (true) {
         System.out.print("Annual Interest Rate: ");
         float annualInterest = scanner.nextFloat();
-        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
-
+        if (annualInterest >= 1 && annualInterest <= 30) {
+            monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+            break;
+        }
+        System.out.println("Please enter a valid value between 1 and 30");
+    }
+    while (true) {
         System.out.print("Period (Years): ");
         byte years = scanner.nextByte();
-        int numberOfPayments = years *MONTHS_IN_YEAR;
-
+        if (years >= 1 && years <= 30) {
+            numberOfPayments = years *MONTHS_IN_YEAR;
+            break;
+        }
+        System.out.println("Please enter a value between 1 and 30");
+    }
         double mortgage = principal
                     * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
                     / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
         String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
-        System.out.println("Mortgage: " + mortgageFormatted);
+        System.out.println("Your approx monthly Mortgage payment would be: " + mortgageFormatted);
     }
 }
